fea14b9763cf8c35384f63ef4569804ce1b7d6a2
Mathieu Carbou <mathieu.carbou@gmail.com>
2025-06-05 19:39:03 +0200

    Only activate WiFi related code when WiFi is available

--- examples/PartitionDownloader/PartitionDownloader.ino
+++ examples/PartitionDownloader/PartitionDownloader.ino
@@ -34,7 +34,7 @@ static AsyncWebServer server(80);
 void setup() {
   Serial.begin(115200);
 
-#ifndef CONFIG_IDF_TARGET_ESP32H2
+#if SOC_WIFI_SUPPORTED || CONFIG_ESP_WIFI_REMOTE_ENABLED
   WiFi.mode(WIFI_AP);
   WiFi.softAP("esp-captive");
 #endif
