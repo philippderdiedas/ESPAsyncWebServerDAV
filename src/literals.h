0a48bafaf92fb0a363815fd3de914646433e1a4a
Jose M. Piñeiro <diosmedepaciencia@gmail.com>
2025-07-18 10:50:30 +0200

    Standardize HTTP headers to Title Case (#233)
    This PR updates all HTTP headers to use Title Case formatting, aligning with RFC 7231 recommendations and ensuring consistency across responses. This change improves readability and aligns our implementation with industry standards. No functional changes were introduced. Only header formatting has been adjusted.
    
    Co-authored-by: Mathieu Carbou <mathieu.carbou@gmail.com>

--- src/literals.h
+++ src/literals.h
@@ -7,45 +7,42 @@ namespace asyncsrv {
 
 static constexpr const char *empty = "";
 
-static constexpr const char *T_inline = "inline";
-static constexpr const char *T_attachment = "attachment; filename=\"%s\"";
-
 static constexpr const char *T__opaque = "\", opaque=\"";
 static constexpr const char *T_100_CONTINUE = "100-continue";
 static constexpr const char *T_13 = "13";
-static constexpr const char *T_ACCEPT = "accept";
-static constexpr const char *T_Accept_Ranges = "accept-ranges";
-static constexpr const char *T_app_xform_urlencoded = "application/x-www-form-urlencoded";
-static constexpr const char *T_AUTH = "authorization";
+static constexpr const char *T_ACCEPT = "Accept";
+static constexpr const char *T_Accept_Ranges = "Accept-Ranges";
+static constexpr const char *T_attachment = "attachment; filename=\"%s\"";
+static constexpr const char *T_AUTH = "Authorization";
 static constexpr const char *T_auth_nonce = "\", qop=\"auth\", nonce=\"";
-static constexpr const char *T_BASIC = "basic";
-static constexpr const char *T_BASIC_REALM = "basic realm=\"";
-static constexpr const char *T_BEARER = "bearer";
+static constexpr const char *T_BASIC = "Basic";
+static constexpr const char *T_BASIC_REALM = "Basic realm=\"";
+static constexpr const char *T_BEARER = "Bearer";
 static constexpr const char *T_BODY = "body";
-static constexpr const char *T_Cache_Control = "cache-control";
+static constexpr const char *T_Cache_Control = "Cache-Control";
 static constexpr const char *T_chunked = "chunked";
 static constexpr const char *T_close = "close";
 static constexpr const char *T_cnonce = "cnonce";
-static constexpr const char *T_Connection = "connection";
-static constexpr const char *T_Content_Disposition = "content-disposition";
-static constexpr const char *T_Content_Encoding = "content-encoding";
-static constexpr const char *T_Content_Length = "content-length";
-static constexpr const char *T_Content_Type = "content-type";
-static constexpr const char *T_Content_Location = "content-location";
-static constexpr const char *T_Cookie = "cookie";
-static constexpr const char *T_CORS_ACAC = "access-control-allow-credentials";
-static constexpr const char *T_CORS_ACAH = "access-control-allow-headers";
-static constexpr const char *T_CORS_ACAM = "access-control-allow-methods";
-static constexpr const char *T_CORS_ACAO = "access-control-allow-origin";
-static constexpr const char *T_CORS_ACMA = "access-control-max-age";
-static constexpr const char *T_CORS_O = "origin";
+static constexpr const char *T_Connection = "Connection";
+static constexpr const char *T_Content_Disposition = "Content-Disposition";
+static constexpr const char *T_Content_Encoding = "Content-Encoding";
+static constexpr const char *T_Content_Length = "Content-Length";
+static constexpr const char *T_Content_Type = "Content-Type";
+static constexpr const char *T_Content_Location = "Content-Location";
+static constexpr const char *T_Cookie = "Cookie";
+static constexpr const char *T_CORS_ACAC = "Access-Control-Allow-Credentials";
+static constexpr const char *T_CORS_ACAH = "Access-Control-Allow-Headers";
+static constexpr const char *T_CORS_ACAM = "Access-Control-Allow-Methods";
+static constexpr const char *T_CORS_ACAO = "Access-Control-Allow-Origin";
+static constexpr const char *T_CORS_ACMA = "Access-Control-Max-Age";
+static constexpr const char *T_CORS_O = "Origin";
 static constexpr const char *T_data_ = "data: ";
-static constexpr const char *T_Date = "date";
-static constexpr const char *T_DIGEST = "digest";
-static constexpr const char *T_DIGEST_ = "digest ";
-static constexpr const char *T_ETag = "etag";
+static constexpr const char *T_Date = "Date";
+static constexpr const char *T_DIGEST = "Digest";
+static constexpr const char *T_DIGEST_ = "Digest ";
+static constexpr const char *T_ETag = "ETag";
 static constexpr const char *T_event_ = "event: ";
-static constexpr const char *T_EXPECT = "expect";
+static constexpr const char *T_EXPECT = "Expect";
 static constexpr const char *T_FALSE = "false";
 static constexpr const char *T_filename = "filename";
 static constexpr const char *T_gzip = "gzip";
@@ -53,12 +50,13 @@ static constexpr const char *T_Host = "host";
 static constexpr const char *T_HTTP_1_0 = "HTTP/1.0";
 static constexpr const char *T_HTTP_100_CONT = "HTTP/1.1 100 Continue\r\n\r\n";
 static constexpr const char *T_id__ = "id: ";
-static constexpr const char *T_IMS = "if-modified-since";
-static constexpr const char *T_INM = "if-none-match";
+static constexpr const char *T_IMS = "If-Modified-Since";
+static constexpr const char *T_INM = "If-None-Match";
+static constexpr const char *T_inline = "inline";
 static constexpr const char *T_keep_alive = "keep-alive";
-static constexpr const char *T_Last_Event_ID = "last-event-id";
-static constexpr const char *T_Last_Modified = "last-modified";
-static constexpr const char *T_LOCATION = "location";
+static constexpr const char *T_Last_Event_ID = "Last-Event-ID";
+static constexpr const char *T_Last_Modified = "Last-Modified";
+static constexpr const char *T_LOCATION = "Location";
 static constexpr const char *T_LOGIN_REQ = "Login Required";
 static constexpr const char *T_MULTIPART_ = "multipart/";
 static constexpr const char *T_name = "name";
@@ -72,21 +70,20 @@ static constexpr const char *T_realm = "realm";
 static constexpr const char *T_realm__ = "realm=\"";
 static constexpr const char *T_response = "response";
 static constexpr const char *T_retry_ = "retry: ";
-static constexpr const char *T_retry_after = "retry-after";
+static constexpr const char *T_retry_after = "Retry-After";
 static constexpr const char *T_nn = "\n\n";
 static constexpr const char *T_rn = "\r\n";
 static constexpr const char *T_rnrn = "\r\n\r\n";
-static constexpr const char *T_Server = "server";
-static constexpr const char *T_Transfer_Encoding = "transfer-encoding";
+static constexpr const char *T_Server = "Server";
+static constexpr const char *T_Transfer_Encoding = "Transfer-Encoding";
 static constexpr const char *T_TRUE = "true";
-static constexpr const char *T_UPGRADE = "upgrade";
+static constexpr const char *T_UPGRADE = "Upgrade";
 static constexpr const char *T_uri = "uri";
 static constexpr const char *T_username = "username";
 static constexpr const char *T_WS = "websocket";
-static constexpr const char *T_WWW_AUTH = "www-authenticate";
+static constexpr const char *T_WWW_AUTH = "WWW-Authenticate";
 
 // HTTP Methods
-
 static constexpr const char *T_ANY = "ANY";
 static constexpr const char *T_GET = "GET";
 static constexpr const char *T_POST = "POST";
@@ -131,6 +128,7 @@ static constexpr const char *T_application_json = "application/json";
 static constexpr const char *T_application_msgpack = "application/msgpack";
 static constexpr const char *T_application_pdf = "application/pdf";
 static constexpr const char *T_application_x_gzip = "application/x-gzip";
+static constexpr const char *T_app_xform_urlencoded = "application/x-www-form-urlencoded";
 static constexpr const char *T_application_zip = "application/zip";
 static constexpr const char *T_font_eot = "font/eot";
 static constexpr const char *T_font_ttf = "font/ttf";
@@ -182,7 +180,7 @@ static constexpr const char *T_HTTP_CODE_412 = "Precondition Failed";
 static constexpr const char *T_HTTP_CODE_413 = "Request Entity Too Large";
 static constexpr const char *T_HTTP_CODE_414 = "Request-URI Too Large";
 static constexpr const char *T_HTTP_CODE_415 = "Unsupported Media Type";
-static constexpr const char *T_HTTP_CODE_416 = "Requested range not satisfiable";
+static constexpr const char *T_HTTP_CODE_416 = "Requested Range Not Satisfiable";
 static constexpr const char *T_HTTP_CODE_417 = "Expectation Failed";
 static constexpr const char *T_HTTP_CODE_429 = "Too Many Requests";
 static constexpr const char *T_HTTP_CODE_500 = "Internal Server Error";
@@ -190,11 +188,14 @@ static constexpr const char *T_HTTP_CODE_501 = "Not Implemented";
 static constexpr const char *T_HTTP_CODE_502 = "Bad Gateway";
 static constexpr const char *T_HTTP_CODE_503 = "Service Unavailable";
 static constexpr const char *T_HTTP_CODE_504 = "Gateway Time-out";
-static constexpr const char *T_HTTP_CODE_505 = "HTTP Version not supported";
+static constexpr const char *T_HTTP_CODE_505 = "HTTP Version Not Supported";
 static constexpr const char *T_HTTP_CODE_ANY = "Unknown code";
 
-static constexpr const uint8_t T_only_once_headers_len = 11;
-static constexpr const char *T_only_once_headers[] = {T_Content_Length,    T_Content_Type,     T_Date,   T_ETag,    T_Last_Modified, T_LOCATION, T_retry_after,
-                                                      T_Transfer_Encoding, T_Content_Location, T_Server, T_WWW_AUTH};
+static constexpr const char *T_only_once_headers[] = {
+  T_Accept_Ranges,     T_Content_Length,   T_Content_Type, T_Connection, T_CORS_ACAC, T_CORS_ACAH,     T_CORS_ACAM, T_CORS_ACAO,
+  T_CORS_ACMA,         T_CORS_O,           T_Date,         T_DIGEST,     T_ETag,      T_Last_Modified, T_LOCATION,  T_retry_after,
+  T_Transfer_Encoding, T_Content_Location, T_Server,       T_WWW_AUTH
+};
+static constexpr size_t T_only_once_headers_len = sizeof(T_only_once_headers) / sizeof(T_only_once_headers[0]);
 
 }  // namespace asyncsrv
